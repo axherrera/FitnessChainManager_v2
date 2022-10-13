ffaa3540c0b9a93afba18cb69cb1b8772c126d5e
Huruy Belay <huruyame@gmail.com>
2022-10-13 10:30:22 -0400

    Rename RunProject2

--- src/fitnesschainmanager/RunProject2.java
+++ src/fitnesschainmanager/RunProject2.java
@@ -4,7 +4,7 @@ package fitnesschainmanager;
  * A class Runproject1 created to run project-1
  * @author ALEJANDRO HERRERA-PINEDA, HURUY BELAY
  */
-public class RunProject1 {
+public class RunProject2 {
     public static void main(String[] args){
         new GymManager().run();
     }
